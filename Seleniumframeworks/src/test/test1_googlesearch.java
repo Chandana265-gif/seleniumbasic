2b28c7daad50c61ff036194a8dc624acdd198d99
Chandana <Chandana@ChandanaSrinivas>
2025-04-03 13:38:24 +0530

    Actual code

--- Seleniumframeworks/src/test/test1_googlesearch.java
+++ Seleniumframeworks/src/test/test1_googlesearch.java
@@ -1,9 +1,11 @@
 package test;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class test1_googlesearch {
 
@@ -20,18 +22,36 @@ public class test1_googlesearch {
         String value="./driverss/chromedriver.exe";
         System.setProperty(key, value);
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://google.com/");
-		driver.findElement(By.id("APjFqb")).sendKeys("BTS");
-//		driver.findElement(By.xpath("//div[@jsmodel=\"b5W85 vNzKHd\"]")).click();
-		driver.findElement(By.name("btnK")).sendKeys(Keys.RETURN);
-		driver.findElement(By.name("btnK")).click();
-		//Thread.sleep(2000);
-		
-		driver.close();
-		
-		System.out.println("Test completed");
-		
-		
-}
+		try {
+            // Open Google
+            driver.get("https://www.google.com/");
+            driver.manage().window().maximize();
+
+            // Wait until the search box is visible
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
+
+            // Enter search query
+            searchBox.sendKeys("BTS");
+
+            // Wait for search button and click
+            WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(By.name("btnK")));
+            searchButton.click();
+
+            // Wait for results to load
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.id("search")));
+
+            // Print page title
+            System.out.println("Page Title: " + driver.getTitle());
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close browser after execution
+        	 System.out.println("Test completed");
+            driver.quit();
+           
+        }
+    }
 
 }
